c5a3f63ca698e425cdb2778dcc1ed85fbfc5e539
AOKKss <44952189+AOKKss@users.noreply.github.com>
2018-11-12 04:11:05 +0100

    Add files via upload

---
+++ Dream.js
@@ -0,0 +1,15 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("511362830185005058")
+setInterval(function() {
+channel.send(`Every champion has felt it every
+victorious person has felt it the urge to quit. Don’t you give up on your dream. I don’t care if you don’t have the money you don’t have the help and you don’t have the family for it and you don’t have the friends for it don’t you give up on your dream don’t’ you do it. It may take you twice as long you may have take courses and classes you might not read as fast you might not move as quick you may not have as much but don’t you quit. If you lay dead even the animals won’t bite you. The risk of being bitten is the cost of getting up. And you have to decide are you so concerned about being bitten that you’re willing to spend the rest of your life laying dead or there is something pumping down inside of you that’s saying bit me or not I’m getting up. I’m gonna be the best me. I’m gonna do all I can do I’m going for it..`);
+}, 25)
+})
+ 
+ 
+client.login('MzU4NjY0OTk5OTE0NDM4NjU4.Dsf33A.RM4w8RJ2IgdRr0tGz9LI8iHPRu4');
